a91e865273da5f1d594904345524148531215912
SharrY111 <145919334+SharrY111@users.noreply.github.com>
2024-07-11 12:08:42 +0500

    Update quiz.py

--- Quiz_app.py/quiz.py
+++ Quiz_app.py/quiz.py
@@ -6,9 +6,23 @@ questions=[
 
 
 def quiz():
+    correc_answers=0
 
     for i in questions:
-        answer=
+        answers=input(i["question"])
+        if answers.lower().strip()==i["answer"].lower():
+            correc_answers+=1
+        
+    total_questios=len(questions)    
+    percentage=(correc_answers/total_questios)*100
+
+
+    print(f"Out of {total_questios} questions you successfully solved {correc_answers}")
+    print(f"Your percentage is {percentage}%")
+
+
+if __name__=="__main__":
+    quiz()
 
 
 
